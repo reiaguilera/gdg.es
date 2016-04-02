2e5c566f90c8632e6a5ee99108e263a971caa644
Abdón Rodríguez Davila <a@abdonrd.com>
2016-04-02 22:32:21 +0200

    Add ESLint and EditorConfig support

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ This code may only be used under the MIT style license found at LICENSE.txt
 'use strict';
 
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var historyApiFallback = require('connect-history-api-fallback');
@@ -31,11 +32,27 @@ var serve = function(baseDir) {
 };
 
 // Serve project and watch files for changes
-gulp.task('serve', function() {
+gulp.task('serve', ['lint'], function() {
   serve(['app']);
 
-  gulp.watch(['app/**/*.html', '!app/bower_components/**/*.html'], reload);
+  gulp.watch(['app/**/*.html', '!app/bower_components/**/*.html'], ['lint', reload]);
   gulp.watch(['app/images/**/*'], reload);
-  gulp.watch(['app/scripts/**/*.js'], reload);
+  gulp.watch(['app/scripts/**/*.js'], ['lint', reload]);
   gulp.watch(['app/styles/**/*.css'], reload);
+});
+
+// Lint JavaScript
+gulp.task('lint', function() {
+  return gulp.src([
+      'app/scripts/**/*.js',
+      'app/elements/**/*.html',
+      'gulpfile.js'
+    ])
+    .pipe(reload({
+      stream: true,
+      once: true
+    }))
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
 });
