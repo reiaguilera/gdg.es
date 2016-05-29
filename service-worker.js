f809facc76c28a8a78550eaf453e1db4abdf178e
Abdón Rodríguez Davila <a@abdonrd.com>
2016-05-29 16:26:04 +0200

    Add licenses

--- service-worker.js
+++ service-worker.js
@@ -3,4 +3,5 @@
  * Copyright (c) 2016 The GDG Spain Authors. All rights reserved.
  * This code may only be used under the MIT style license found at http://gdg.es/LICENSE.txt
  */
+
 console.info('Service worker disabled for development, will be generated at build time.');
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (c) 2016 The GDG Spain Authors. All rights reserved.
+ * This code may only be used under the MIT style license found at http://gdg.es/LICENSE.txt
+ */
+
 module.exports = {
   staticFileGlobs: [
     '/index.html',
