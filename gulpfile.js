c6aa61e53f679aeefaf665b44784d2addf3996c8
Abdón Rodríguez Davila <a@abdonrd.com>
2016-08-18 01:58:10 +0200

    Add the swPrecacheConfig options

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,27 @@ global.config = {
   },
   serviceWorkerPath: 'service-worker.js',
   swPrecacheConfig: {
-    navigateFallback: '/index.html'
+    staticFileGlobs: [
+      '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+      '/images/**/*',
+      '/scripts/**/*',
+      '/index.html',
+      '/manifest.json'
+    ],
+    navigateFallback: '/index.html',
+    navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+    runtimeCaching: [
+      {
+        urlPattern: /\/data\/images\/.*/,
+        handler: 'cacheFirst',
+        options: {
+          cache: {
+            maxEntries: 50,
+            name: 'items-cache'
+          }
+        }
+      }
+    ]
   },
   filesToLint: [
     'scripts/**/*.js',
